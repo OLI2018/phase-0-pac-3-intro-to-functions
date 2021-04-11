bdff1ae748126a8fd41e6aea7027e715693e8493
Oleg <roxvemm2014@gmail.com>
2021-04-11 14:24:18 -0700

    Done with this assignment!

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Follow along with the examples here
+function doNothing() {} 
+
+function sayHello() {
+    console.log("Hello"); 
+    }
+
+sayHello(); 
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!"); 
+}
+
+sayHelloToIsabel(); 
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofai!");
+}
+
+sayHelloToSofia(); 
+
+function sayHelloTo(firstName) {
+console.log(`Hello, ${firstName}`); 
+}
+
+sayHelloTo('Isabel'); 
+sayHelloTo("Jane"); 
+sayHelloTo("R2-D2"); 
+sayHelloTo(1); 
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}`); 
+}
+
+say("Goodbye", "Julio"); 
+
+function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  } 
+
+  function add (x, y) {
+      return x + y; 
+  }
+
+   
+  console.log(add (1, 2)); 
+
+  function say (greeting, firstName) {
+return `${greeting}, ${firstName}!`
+  }
+
+  console.log(say("Hello", "Sofia"));
+
+  
+
+
